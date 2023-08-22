1ca32540e7bccde98d5e78f6a87d9183b03c891a
Eugeneatsu <62382854+Eugeneatsu@users.noreply.github.com>
2023-08-22 23:32:06 +0000

    Update functions.c

--- functions.c
+++ functions.c
@@ -1,5 +1,5 @@
 #include "main.h"
-//This is the comment you should make
+/* This is the comment you should make */
 /************************* PRINT CHAR *************************/
 // another one
 /**
